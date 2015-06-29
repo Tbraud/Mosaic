a46ec5ca7424eb89b6894986ac6eab92761dab2e
Tristan <tristan@skyros.imag.fr>
2015-06-29 17:27:41 +0200

    Added License

--- mosaic_screensaver.py
+++ mosaic_screensaver.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python
+
+#        DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#                    Version 2, December 2004 
+#
+# Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+#
+# Everyone is permitted to copy and distribute verbatim or modified 
+# copies of this license document, and changing it is allowed as long 
+# as the name is changed. 
+#
+#            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+#   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+#
+#  0. You just DO WHAT THE FUCK YOU WANT TO.
+
 import pygame
 from pygame.constants import *
 import sys,os,re
-import time
 import mimetypes
 import random
 from subprocess import Popen,PIPE
