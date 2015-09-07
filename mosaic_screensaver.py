2af5adc92b8a537b91daa8b68636c63c4a29aa6f
Tristan <tristan@skyros.imag.fr>
2015-09-07 12:25:40 +0200

    Multiprocess

--- mosaic_screensaver.py
+++ mosaic_screensaver.py
@@ -15,11 +15,14 @@
 #  0. You just DO WHAT THE FUCK YOU WANT TO.
 
 import pygame
-from pygame.constants import *
+from pygame.locals import *
 import sys,os,re
 import mimetypes
 import random
 from subprocess import Popen,PIPE
+import signal
+import multiprocessing
+from multiprocessing import Process, Queue
 
 
 def getScreenInfo(windowid):
@@ -50,8 +53,8 @@ def initlist():
     return imglist
 
 def initscreen():
+    # Here comes the magic
     flags=pygame.DOUBLEBUF
-    #flags=0
     if '--root' in sys.argv or '-root' in sys.argv:
         print "root"
         windowid=os.environ.get('XSCREENSAVER_WINDOW')
@@ -74,51 +77,116 @@ def initscreen():
     pygame.mouse.set_visible(False)
     return screen,width,height
 
-def initcontainers(width,height):
-    containers=[]
+def isincontainer(((i,j),(width,height)),contlist):
+    for cont in contlist:
+        if ((i > cont[0][0] and i < cont[0][0]+cont[1][0]) \
+            or (i+width > cont[0][0] and i+width<cont[0][0]+cont[1][0])) \
+            and( (j > cont[0][1] and j < cont[0][1]+cont[1][1]) \
+            or (j+height > cont[0][1] and j+height < cont[0][1]+cont[1][1])):
+                return True
+        else:
+            return False
+    return False
 
-    for i in range(0,width,width/4): 
-        for j in range(0,height,height/4):
-            if i<width and j<height:
-                containers.append(((i,j),(i+width/4,j+height/4)))
+def initcontainers(coef):
+    containers=[]
+    bigcont=[]
+
+    # First, init the big containers
+    nbig=coef*coef/16
+    for i in range(nbig):
+        it=random.randint(0,coef-2)
+        jt=random.randint(0,coef-2)
+        # We check that it doesn't overlap with another big container
+        if not isincontainer(((it,jt),(2,2)),bigcont):
+            bigcont.append(((it,jt),(2,2)))
+        
+    # Init the small containers
+    for i in range(0,coef): 
+        for j in range(0,coef):
+            # We check that it doesn't overlap with a big container
+            if not isincontainer(((i,j),(1,1)),bigcont):
+                containers.append(((i,j),(1,1)))
+
+    for cont in bigcont:
+        containers.append(cont)
     return containers
 
 
-def mosaic(imglist,containers):
-    it=random.randint(0, len(imglist)-1)
-    img = pygame.image.load(imglist[it]).convert()
-    imgsizex,imgsizey= img.get_rect().size
-    it2=random.randint(0,len(containers)-1)
-    cont=containers[it2]
-    posx=cont[0][0]
-    posy=cont[0][1]
-    lenx=cont[1][0]-cont[0][0]
-    leny=cont[1][1]-cont[0][1]
-    img = pygame.transform.scale(img,(lenx,leny))
-    return img,posx,posy
+def mosaic(imglist,containers,width,height,coef):
+    # It's the worker process, we define the sigterm handler
+    signal.signal(signal.SIGTERM , sigterm_handler)
+    # Main loop
+    while True:
+        # pick a random container to put the image in
+        it2=random.randint(0,len(containers)-1)
+        cont=containers[it2]
+        posx=cont[0][0]*width/coef
+        posy=cont[0][1]*height/coef
+        lenx=cont[1][0]*width/coef
+        leny=cont[1][1]*height/coef
+
+        # pick a random image and load it
+        imgsizex=0;imgsizey=0;
+        # We check that the image is not too small (which would make it ugly
+        # when resizing)
+        while imgsizex<lenx or imgsizey<leny:
+            it=random.randint(0, len(imglist)-1)
+            img = pygame.image.load(imglist[it]).convert()
+            imgsizex,imgsizey= img.get_rect().size
+
+
+        # Select how we will crop the image (according to its size ratio)
+        coefx=imgsizex/lenx
+        coefy=imgsizey/leny
+        if coefx>coefy:
+            img=img.subsurface(imgsizex/2-imgsizex/coefx/2,0,imgsizex/2+imgsizex/coefx/2,imgsizey)
+        else:
+            img=img.subsurface(0,imgsizey/2-imgsizey/coefy/2,imgsizex,imgsizey/2+imgsizey/coefy/2)
+
+        # Scale the cropped image to the surface
+        img = pygame.transform.scale(img,(lenx,leny))
+        q.put((pygame.image.tostring(img,"RGB"),posx,posy,lenx,leny))
 
 def draw(screen,img,posx,posy):
-    screen.blit(img, (posx,posy))
+    screen.blit(img, (posx, posy))
     pygame.display.flip()
 
 def main():
     imglist=initlist()
     screen,width,height=initscreen()
-    cont=initcontainers(width,height)
+    coef=4
+    cont=initcontainers(coef)
+    counter=0
+    # Worker process : loads and resizes images
+    p=[Process(target=mosaic, args=(imglist,cont,width,height,coef)) \
+            for i in range(multiprocessing.cpu_count()-1)]
+    [proc.start() for proc in p]
     while True:
-        img,posx,posy=mosaic(imglist,cont)
+        # Operations needed for changing containers over time
+        counter+=1
+        if counter>100:
+            counter=0
+            cont=initcontainers(coef)
+        # Get processed image
+        imglist,posx,posy,lenx,leny=q.get()
+        img=pygame.image.frombuffer( imglist, (lenx,leny), 'RGB' )
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type ==KEYUP:
-                print "Keyup"
+            if event.type in [pygame.QUIT, pygame.KEYUP]:
+                [proc.terminate() for proc in p]
                 pygame.quit()
                 sys.exit()
         pygame.time.wait(1000) 
         draw(screen,img,posx,posy)
 
 
+def sigterm_handler(_signo, _stack_frame):
+    # Don't wait for the queue to be empty to close process
+    q.cancel_join_thread()
+    # Exit gracefully
+    sys.exit(0)
 
 if __name__=='__main__':
-	main()
+    # The queue used for sharing images among processes
+    q = Queue(maxsize=10)
+    main()
